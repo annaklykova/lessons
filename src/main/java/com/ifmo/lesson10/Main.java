bc0de5739ac1aab2632ddada2025c53725f79862
annaklykova <klykova1985@gmail.com>
2019-11-08 20:25:55 +0300

    hw iterable

---
+++ src/main/java/com/ifmo/lesson10/Main.java
@@ -0,0 +1,15 @@
+package com.ifmo.lesson10;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>();
+        List<String> linkedList = new LinkedList<>();
+
+
+    }
+}
